4c26e3e256e0b03bbe18613f1da3fa4dafcb5a30
areeba-noreen <arebanoreen99@yahoo.com>
2023-06-02 15:29:16 +0500

    login form created

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+if(username=="admin" && password=="text"){
+    header('Location: http://localhost/basephp/dashboard.php?username=' . $username);
+    exit;
+}
+
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="form-wrapper">
+            <form action="">
+                <div class="field-group">
+                    <div>
+                        <label class="form-label">User Name</label>
+                        <input class="form-input" type="name" name="username"/>
+                    </div>
+                    <div>
+                        <label class="form-label">Password &nbsp;</label>
+                        <input class="form-input" type="password" name="password"/>
+                    </div>
+                    <div>
+                        <input class="btn" type="submit" value="Login" name=""/>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
